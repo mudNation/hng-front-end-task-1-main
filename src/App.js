ff34c8c950afb6daa1e839ff2d82fed590c5ef69
Osusu Osusu <osusu4@gmail.com>
2022-11-01 22:29:10 +0100

    abc

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="pb-5">Linking trees</h1>
+        <h1 className="pb-5">Osusu's Profile</h1>
         <img id="profile__img" src={osusu} className="App-logo mb-2 img-fluid border rounded-circle img-thumbnail" alt="logo" />
         <p id="twitter">
           Twitter: Osusu_V_O
@@ -15,7 +15,7 @@ function App() {
         </p>
 
         <a
-          className="btn btn-info mb-2"
+          className="btn btn-danger mb-2"
           id='btn__zuri'
           href="https://training.zuri.team/"
           target="_blank"
@@ -24,25 +24,25 @@ function App() {
           Zuri
         </a>
         <a
-          className="btn btn-info mb-2"
+          className="btn btn-danger mb-2"
           href="http://books.zuri.team/"
           id="books"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Books for design and coding
+          My Books for design and codings
         </a>
         <a
-          className="btn btn-info mb-2"
+          className="btn btn-danger mb-2"
           id='book__python'
           href="https://books.zuri.team/python-for-beginners?ref_id=Ayibanimi+Ikoli"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Great reads for sale
+          Great reads recommended by me
         </a>
         <a
-          className="btn btn-info mb-2"
+          className="btn btn-danger mb-2"
           id='"pitch'
           href="https://background.zuri.team/"
           target="_blank"
@@ -51,13 +51,13 @@ function App() {
           Background check your devs
         </a>
         <a
-          className="btn btn-info mb-5"
+          className="btn btn-danger mb-5"
           id="book__design"
           href="https://books.zuri.team/design-rules"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Free design book you definetely want to get
+          Paid books written by me
         </a>
       </header>
     </div>
